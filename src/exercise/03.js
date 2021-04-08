4e356cb9d79b35edf34bd25f3798033176eb25c2
Garrett Schroath <gtschroath@gmail.com>
2021-04-08 07:10:09 -0500

    ex 3 ec 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -56,7 +56,16 @@ function ListItem({
     />
   )
 }
-ListItem = React.memo(ListItem)
+ListItem = React.memo(ListItem, (prevProps, nextProps) => {
+  if (prevProps.getItemProps !== nextProps.getItemProps) return false
+  if (prevProps.item !== nextProps.item) return false
+  if (prevProps.index !== nextProps.index) return false
+  if (prevProps.selectedItem !== nextProps.selectedItem) return false
+  if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
+    return prevProps.index !== nextProps.highlightedIndex && nextProps.index !== nextProps.highlightedIndex
+  }
+  return true
+})
 
 function App() {
   const forceRerender = useForceRerender()
